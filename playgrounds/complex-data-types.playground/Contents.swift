fe3e5d5047372981448f2c5acb2c0966857e3b4b
Charlie Wallin <charliekwallin@gmail.com>
2020-01-22 16:38:47 -0500

    Updated day2 and tried adding a playground

---
+++ playgrounds/complex-data-types.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+let mandolin = "Peggy"
+let whistle = "Hug"
+let banjo = "Tim"
+let fiddle = "Philip"
+let concertina = "Charlie"
+
+let session = [mandolin, whistle, banjo, fiddle, concertina]
+
+print(session)
+
+
