0c48893939cd5b0e52a076da5213458507c34bd5
Charlie Wallin <charliekwallin@gmail.com>
2020-01-26 06:48:35 -0500

    Updated _posts/2020-01-22-swift-day-002.markdown and playgrounds/complex-data-types.playground/Contents.swift

--- playgrounds/complex-data-types.playground/Contents.swift
+++ playgrounds/complex-data-types.playground/Contents.swift
@@ -1,13 +1,48 @@
 import UIKit
 
+// Arrays
+
 let mandolin = "Peggy"
 let whistle = "Hug"
 let banjo = "Tim"
 let fiddle = "Philip"
 let concertina = "Charlie"
 
-let session = [mandolin, whistle, banjo, fiddle, concertina]
+let sessionInstruments = [mandolin, whistle, banjo, fiddle, concertina]
 
-print(session)
+print(sessionInstruments)
+
+// Sets
+
+let instruments = Set(["Fiddle", "Concertina", "Whistle"])
+
+print(instruments)
+
+let costOfInstruments = [
+    "concertina": 5_500,
+    "whistle": 2_000
+]
+
+print(costOfInstruments)
+
+// Enums
+
+enum Compass {
+    case north
+    case east
+    case south
+    case west
+}
+
+var direction = Compass.north
+print(direction)
+
+
+
+
+
+
+
+
 
 
