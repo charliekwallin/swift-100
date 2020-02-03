bad148149578f0ac486bf84e70b42b1ca5d26a26
Charlie Wallin <charliekwallin@gmail.com>
2020-02-03 06:51:09 -0500

    _posts/2020-02-02-swiftui-0004.markdown playgrounds/loops.playground/Contents.swift playgrounds/loops.playground/contents.xcplayground playgrounds/loops.playground/playground.xcworkspace/xcuserdata/charleskwallin.xcuserdatad/UserInterfaceState.xcuserstate

--- playgrounds/loops.playground/Contents.swift
+++ playgrounds/loops.playground/Contents.swift
@@ -13,6 +13,57 @@ for number in count {
 
 /*:
  ## while
+ 
+ Using his examples but not trying to copy versbating, just thinking through it
  */
 
+var number = 1
+
+while (number <= 20) {
+    number += 1
+}
+
+print ("Ready or not, here I come.")
+
+repeat {
+    print("This is false")
+} while false
+
  
+outerLoop: for i in 1...10 {
+    for j in 1...10 {
+        let product = i * j
+        print ("\(i) * \(j) is \(product)")
+
+        if product == 50 {
+            print("It's a bullseye!")
+            break outerLoop
+        }
+    }
+}
+
+// using the continue statement
+
+for i in 0...9 {
+    if i % 2 == 1 {
+        continue
+    }
+    
+    print(i)
+}
+
+// lets try it with weekdays
+
+let daysInWeek = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+    ]
+
+for day in daysInWeek {
+    print("\(day)")
+}
