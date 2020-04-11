5225f273afb8c4046f65dda605c95487060362fb
Charlie Wallin <charliekwallin@gmail.com>
2020-04-11 09:25:39 -0400

    Modified _posts/2020-03-19-swiftui-day-017.markdown

--- projects/ElementQuiz/ElementQuiz/ViewController.swift
+++ projects/ElementQuiz/ElementQuiz/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    let elementList = ["Carbon", "Gold", "Chlorine", "Sodium"]
+    var currentElementIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateElement()
+    }
+    
+    func updateElement() {
+        answerLabel.text = "?"
+        let elementName = elementList[currentElementIndex]
+        let image = UIImage(named: elementName)
+        imageView.image = image
     }
 
-
+    @IBAction func showAnswer(_ sender: UIButton) {
+        answerLabel.text = elementList[currentElementIndex]
+    }
+    
+    
+    @IBAction func gotoNextElement(_ sender: UIButton) {
+        currentElementIndex += 1
+        if currentElementIndex >= elementList.count {
+            currentElementIndex = 0
+        }
+        updateElement()
+    }
 }
 
